a109575ee3bdd9680ad14881ae9ad665a2c93eb6
nonsobiose <slimbion@gmail.com>
2018-02-18 22:56:54 +0100

    Created Layouts, implemented menu item and also parent activity nav

---
+++ app/src/main/java/com/nonsobiose/unavailable/MessagesActivity.java
@@ -0,0 +1,28 @@
+package com.nonsobiose.unavailable;
+
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MessagesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Code to activate upwards navigation to parent Activity
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
